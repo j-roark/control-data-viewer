e5fae0e8436f0ff7fc3fb2f9be908f5b02a83b6a
j-roark <65353891+j-roark@users.noreply.github.com>
2020-08-18 18:07:44 -0400

    Update write_to_excel.py
    catch os errors, print traceback. since this is the last step in the program, I won't worry about breaking the main thread.

--- src/write_to_excel.py
+++ src/write_to_excel.py
@@ -16,10 +16,19 @@ def write_to_excel(final_data):
     today = date.today()
     filename = f'E:/Backup/{random.randint(1000, 9999)}_{today.strftime("%b-%d-%Y")}_package_stats_by_order_number.xlsx'
     
-    # Filename and directory to write the .xlsx file too.
+    # Filename and directory to write the .xlsx file to.
     
-    with pd.ExcelWriter(filename) as writer:
-        final_data.to_excel(writer, sheet_name="Pack. Stats. by Order Num", index=False)
+    try:
+     
+        with pd.ExcelWriter(filename) as writer:
+            final_data.to_excel(writer, sheet_name="Pack. Stats. by Order Num", index=False)
 
-    print(f'Saved as: {filename}')
-    return
+        print(f'Saved as: {filename}')
+        return
+    
+    except OSError as e:
+         
+        print("Error: the target directory likely doesn't exist or, you don't have access to write to it. \n Traceback: ")
+        print(e)
+        return
+          
